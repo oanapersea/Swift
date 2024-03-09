8666fe2470399dbc8191fdd533e23453b71c6f09
oanapersea <115214117+oanapersea@users.noreply.github.com>
2024-03-09 14:58:26 +0200

    Add files via upload

---
+++ warcardgame/warcardgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  warcardgameApp.swift
+//  warcardgame
+//
+//  Created by Persea Oana on 01.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct warcardgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
